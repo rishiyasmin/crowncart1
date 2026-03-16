6f4466797e9f74c0ec0a2fefd9f4ccef1140bac6
rishiyasmin <23bq1a4784@gmail.com>
2026-03-16 14:23:47 +0530

    fixed

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -28,7 +28,7 @@ export default function Signup() {
   }
 
   try {
-    const res = await fetch("http://localhost:5000/api/auth/signup", {
+    const res = await fetch("https://crowncart1.onrender.com", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ name, email, password }),
